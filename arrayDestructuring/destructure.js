79c4b3b3b94cf6a2f9b48fb7f61557cfa72c3a81
dishant <dishanthegde@gmail.com>
2026-08-24 14:01:26 +0530

    "json methods"

--- arrayDestructuring/destructure.js
+++ arrayDestructuring/destructure.js
@@ -1,49 +1,49 @@
-// let colors = ['red', 'green', 'blue', 'brown', 'white']
-// let [r,g,b,br,w] = colors
-// console.log(br);
+let colors = ['red', 'green', 'blue', 'brown', 'white']
+let [r,g,b,br,w] = colors
+console.log(br);
 
-// let [,,bl,,wh] = colors
-// console.log(bl)
-// console.log(wh);
+let [,,bl,,wh] = colors
+console.log(bl)
+console.log(wh);
 
-// let [a, c, d] = [10, 20];
-// console.log(a,c, d=30);
-
-
-// let x = 10, y = 30;
-
-// [y,x] = [x,y]
-// console.log(x,y);
-
-// let array = ['a','b','c',['d','e']];
-// let [p,q,r,s,t] = array;
-// console.log(s[1]);
-
-// console.log(t);
-
-// let userData = {
-//     name : "dth",
-//     id : 101,
-//     city: "bangalore",
-//     address: {
-//         street : "church street",
-//         homeNo : "#123"
-//     }
-// }
-
-// let destr = {name, id, city, address:{street, homeNo}} = userData
+let [a, c, d] = [10, 20];
+console.log(a,c, d=30);
 
 
-// console.log(street);
-// console.log(homeNo);
+let x = 10, y = 30;
 
-// let newDest = {name:realName, id:e_id, city:realCity, address:{street:realStreet, homeNo:realHomeNo}} = userData
-// console.log(realStreet);
-// console.log(realCity);
+[y,x] = [x,y]
+console.log(x,y);
 
-// let destr = {name, id, city, address:{street, homeNo}, number=12312414} = userData
+let array = ['a','b','c',['d','e']];
+let [p,q,r,s,t] = array;
+console.log(s[1]);
 
-// console.log(number);
+console.log(t);
+
+let userData = {
+    name : "dth",
+    id : 101,
+    city: "bangalore",
+    address: {
+        street : "church street",
+        homeNo : "#123"
+    }
+}
+
+let destr = {name, id, city, address:{street, homeNo}} = userData
+
+
+console.log(street);
+console.log(homeNo);
+
+let newDest = {name:realName, id:e_id, city:realCity, address:{street:realStreet, homeNo:realHomeNo}} = userData
+console.log(realStreet);
+console.log(realCity);
+
+let destr = {name, id, city, address:{street, homeNo}, number=12312414} = userData
+
+console.log(number);
 
 let userData = [{
     id:1,
